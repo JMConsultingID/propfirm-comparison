6e646b0418a9e4634f4ea52cfa69f667d6bf60f3
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-18 14:32:48 +0800

    adding elementor widget

---
+++ includes/class-propfirm-comparison-elementor.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * The file that defines the core plugin class
+ *
+ * A class definition that includes attributes and functions used across both the
+ * public-facing side of the site and the admin area.
+ *
+ * @link       https://fundedtrading.com
+ * @since      1.0.0
+ *
+ * @package    Propfirm_Comparison
+ * @subpackage Propfirm_Comparison/includes
+ */
+
+/**
+ * The core plugin class.
+ *
+ * This is used to define internationalization, admin-specific hooks, and
+ * public-facing site hooks.
+ *
+ * Also maintains the unique identifier of this plugin as well as the current
+ * version of the plugin.
+ *
+ * @since      1.0.0
+ * @package    Propfirm_Comparison
+ * @subpackage Propfirm_Comparison/includes
+ * @author     Ardika JM Consulting <ardi@jm-consulting.id>
+ */
+function add_propfirm_comparison_categories( $elements_manager ) {
+
+    $elements_manager->add_category(
+        'propfirm-comparison-category',
+        [
+            'title' => esc_html__( 'Funded Trading Widget', 'propfirm-comparison' ),
+            'icon' => 'fa fa-plug',
+        ]
+    );
+}
+add_action( 'elementor/elements/categories_registered', 'add_propfirm_comparison_categories' );
+
+function register_propfirm_comparison_widget( $widgets_manager ) {
+
+    require_once( __DIR__ . '/widgets/propfirm-comparison-widget-list-propfirm.php' );    
+
+    $widgets_manager->register( new \Elementor_PropfirmComparison_Widget_listPropfirm() );
+}
+add_action( 'elementor/widgets/register', 'register_propfirm_comparison_widget' );
