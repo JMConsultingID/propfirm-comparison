0c627deaa3704c0ed91fa186d56c2ff530f7abd2
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-19 14:05:42 +0800

    Update propfirm-comparison-widget-compare-propfirm.php

--- includes/widgets/propfirm-comparison-widget-compare-propfirm.php
+++ includes/widgets/propfirm-comparison-widget-compare-propfirm.php
@@ -93,7 +93,6 @@ class Elementor_PropfirmComparison_Widget_comparePropfirm extends \Elementor\Wid
 			// Loop through $propfirm_ids to get data and build the comparison table
 			if (empty($propfirm_ids)) {
     			echo "No PropFirm data available for comparison. this is for example";
-    			$propfirm_ids = array(400,401);
     		} else {
 
     		// Fetch necessary data for each propfirm (replace this with your actual code)
@@ -113,6 +112,7 @@ class Elementor_PropfirmComparison_Widget_comparePropfirm extends \Elementor\Wid
 
     		?>
 			<h1>Comparison Results</h1>
+			<p><?php echo $propfirm_ids; ?></p>
 			<table class="table table-bordered table-success table-striped table-hover">
 			    <thead>
 			        <tr>
