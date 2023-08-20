b96f77a5a345f07dfc87bca71bb380d2dd341a4e
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-21 00:22:53 +0800

    Update propfirm-comparison-widget-compare-propfirm.php

--- includes/widgets/propfirm-comparison-widget-compare-propfirm.php
+++ includes/widgets/propfirm-comparison-widget-compare-propfirm.php
@@ -87,8 +87,15 @@ class Elementor_PropfirmComparison_Widget_comparePropfirm extends \Elementor\Wid
 		<div class="col-12">			
 			<h1><?php echo $propfirm_heading_title; ?></h1>
 			<?php
-			$propfirm_ids = isset($_SESSION['compare_list']) && is_array($_SESSION['compare_list']) ? $_SESSION['compare_list'] : array();
-    		$propfirm_id = !empty($propfirm_ids) ? intval($propfirm_ids[0]) : 0;
+			$propfirm_slugs = isset($_GET['propfirm_ids']) ? explode('-vs-', $_GET['propfirm_ids']) : array();
+			$propfirm_ids = array();
+
+			foreach ($propfirm_slugs as $slug) {
+			    $propfirm_post = get_page_by_path($slug, OBJECT, 'post'); // Replace 'post' with your custom post type if needed
+			    if ($propfirm_post) {
+			        $propfirm_ids[] = $propfirm_post->ID;
+			    }
+			}
 
             $options = get_option('propfirm_comparison_settings');
             $selected_post_type = isset($options['propfirm_comparison_post_type']) ? sanitize_text_field($options['propfirm_comparison_post_type']) : 'propfirm';
