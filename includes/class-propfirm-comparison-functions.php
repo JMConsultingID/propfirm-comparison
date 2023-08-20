198060229865d7b994a46e42029a2ffd6ecf51ab
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-20 18:03:22 +0800

    Update class-propfirm-comparison-functions.php

--- includes/class-propfirm-comparison-functions.php
+++ includes/class-propfirm-comparison-functions.php
@@ -181,7 +181,40 @@ function add_offcanvas_comparasion_to_footer() {
       </div>
       <div class="offcanvas-body">
         <div class="compare-sidebar">
-        
+        ="list-group mb-3">
+          <li class="list-group-item d-flex justify-content-between lh-sm">
+            <div>
+              <h6 class="my-0">Product name</h6>
+              <small class="text-muted">Brief description</small>
+            </div>
+            <span class="text-muted">$12</span>
+          </li>
+          <li class="list-group-item d-flex justify-content-between lh-sm">
+            <div>
+              <h6 class="my-0">Second product</h6>
+              <small class="text-muted">Brief description</small>
+            </div>
+            <span class="text-muted">$8</span>
+          </li>
+          <li class="list-group-item d-flex justify-content-between lh-sm">
+            <div>
+              <h6 class="my-0">Third item</h6>
+              <small class="text-muted">Brief description</small>
+            </div>
+            <span class="text-muted">$5</span>
+          </li>
+          <li class="list-group-item d-flex justify-content-between bg-light">
+            <div class="text-success">
+              <h6 class="my-0">Promo code</h6>
+              <small>EXAMPLECODE</small>
+            </div>
+            <span class="text-success">−$5</span>
+          </li>
+          <li class="list-group-item d-flex justify-content-between">
+            <span>Total (USD)</span>
+            <strong>$20</strong>
+          </li>
+        </ul>
         <div id="compare-list" class="row"></div>
         <div class="d-grid gap-2">
         <?php
