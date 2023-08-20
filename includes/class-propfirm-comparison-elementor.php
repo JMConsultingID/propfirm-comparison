7442d329b2b27641ed269012cf195c883939d3a7
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-20 12:08:25 +0800

    Update class-propfirm-comparison-elementor.php

--- includes/class-propfirm-comparison-elementor.php
+++ includes/class-propfirm-comparison-elementor.php
@@ -49,10 +49,14 @@ function register_propfirm_comparison_widget( $widgets_manager ) {
 }
 add_action( 'elementor/widgets/register', 'register_propfirm_comparison_widget' );
 
-function elementor_test_widgets_dependencies() {
-    /* Scripts */
-    wp_register_script( 'propfirm-comparison-widget-bootstrap-script', plugins_url( '../public/js/bootstrap.bundle.min.js', __FILE__ ) );
-    /* Styles */
-    wp_register_style( 'propfirm-comparison-widget-bootstrap-style', plugins_url( '../public/css/bootstrap-grid.min.css', __FILE__ ), false );
+function propfirm_comparison_frontend_stylesheets() {
+    wp_register_style( 'propfirm-comparison-widget-bootstrap-style', plugins_url( '../public/css/bootstrap-grid.min.css', __FILE__ ) );
+    wp_enqueue_style( 'propfirm-comparison-widget-bootstrap-style' );
 }
-add_action( 'wp_enqueue_scripts', 'elementor_test_widgets_dependencies' );
+add_action( 'elementor/frontend/before_enqueue_styles', 'propfirm_comparison_frontend_stylesheets' );
+
+function propfirm_comparison_frontend_scripts() {
+    wp_register_script( 'propfirm-comparison-widget-bootstrap-script', plugins_url( '../public/js/bootstrap.bundle.min.js', __FILE__ ) );
+    wp_enqueue_script( 'propfirm-comparison-widget-bootstrap-script' );
+}
+add_action( 'elementor/frontend/before_register_scripts', 'propfirm_comparison_frontend_scripts' );
